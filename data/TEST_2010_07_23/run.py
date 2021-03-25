a0cac3fd2683af93e5650804ac98c29a5e39dd17
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-03-25 09:12:37 +0100

    Remove unused data and doc build files

--- data/TEST_2010_07_23/run.py
+++
@@ -1,7 +0,0 @@
-from time import perf_counter as timer
-import runpy
-
-tic = timer()
-runpy.run_module('pympa', run_name='__main__')
-toc = timer()
-print(f"Elapsed time: {toc - tic:.2f} seconds.")
