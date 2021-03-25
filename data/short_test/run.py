e3d828d4ed343532fc6530496e68d61a0aedb2e5
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-03-25 11:33:21 +0100

    Use longer test than before, move script, launch Dask local cluster

--- data/short_test/run.py
+++ data/short_test/run.py
@@ -1,7 +1,15 @@
 from time import perf_counter as timer
-import runpy
+from dask.distributed import LocalCluster
+from subprocess import run
 
-tic = timer()
-runpy.run_module('pympa', run_name='__main__')
-toc = timer()
-print(f"Elapsed time: {toc - tic:.2f} seconds.")
+if __name__ == '__main__':
+     cluster = LocalCluster()
+     address = cluster.scheduler_address
+
+     tic = timer()
+     run(['./bin/pympa',
+          'data/short_test/travel_times', 'data/short_test/templates',
+          'data/short_test/templates.zmap', 'data/short_test/continuous',
+          'data/short_test/settings.yaml', 'data/short_test/output.stats', address])
+     toc = timer()
+     print(f"Elapsed time: {toc - tic:.2f} seconds.")
